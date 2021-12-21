fded1c2b32d8f25a8ce223f993e91d090642d6ec
Vinicius <viniciusiandrade@hotmail.com>
2021-12-20 23:11:29 -0300

    Counter Users

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const fields = document.querySelectorAll('#form-user-create [name]');
 const obj = {};
 
+
+const counterRows = () => {
+    const counterUsers = document.querySelector('#counter-users')
+    const tbody = document.querySelector('#table-users').rows.length
+
+    counterUsers.innerHTML = tbody;
+    console.log(tbody)
+
+}
+
 const addLine = (dataUser) => {
     let lineTable = document.createElement('tr');
     lineTable.innerHTML = `
-        <tr>
-            <td>
-                <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-            </td>
-            <td>${obj.name}</td>
-            <td>${obj.email}</td>
-            <td>${obj.admin}</td>
-            <td>${obj.birth}</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>`
-
+    <tr>
+    <td>
+    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+    </td>
+    <td>${obj.name}</td>
+    <td>${obj.email}</td>
+    <td>${obj.admin}</td>
+    <td>${obj.birth}</td>
+    <td>
+    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+    </tr>`
+    
     document.getElementById('table-users').appendChild(lineTable);
-
+    
 }
 
 
@@ -38,10 +48,10 @@ document.getElementById('form-user-create').addEventListener('submit', (event) =
     });
     
     addLine(obj)
+    counterRows();
     // console.log(obj)
     
 });
 
 
-    
-    
+
