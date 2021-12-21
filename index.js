e171ad14a0e098d39aeca0819128206a02ed1603
Vinicius <viniciusiandrade@hotmail.com>
2021-12-21 16:55:22 -0300

    Class User

--- index.js
+++ index.js
@@ -7,31 +7,48 @@ const counterRows = () => {
     const tbody = document.querySelector('#table-users').rows.length
 
     counterUsers.innerHTML = tbody;
-    console.log(tbody)
 
 }
 
 const addLine = (dataUser) => {
+    // console.log(dataUser)
+
     let lineTable = document.createElement('tr');
     lineTable.innerHTML = `
     <tr>
-    <td>
-    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-    </td>
-    <td>${obj.name}</td>
-    <td>${obj.email}</td>
-    <td>${obj.admin}</td>
-    <td>${obj.birth}</td>
-    <td>
-    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-    </td>
+        <td>
+            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+        </td>
+        <td>${obj.name}</td>
+        <td>${obj.email}</td>
+        <td>${obj.admin}</td>
+        <td>${obj.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
     </tr>`
     
     document.getElementById('table-users').appendChild(lineTable);
-    
-}
 
+
+
+    // document.getElementById('table-users').innerHTML= `
+    // <tr>
+    //     <td>
+    //         <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+    //     </td>
+    //     <td>${obj.name}</td>
+    //     <td>${obj.email}</td>
+    //     <td>${obj.admin}</td>
+    //     <td>${obj.birth}</td>
+    //     <td>
+    //         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+    //         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    //     </td>
+    // </tr>`
+
+}
 
 document.getElementById('form-user-create').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -47,10 +64,19 @@ document.getElementById('form-user-create').addEventListener('submit', (event) =
         }
     });
     
-    addLine(obj)
-    counterRows();
-    // console.log(obj)
-    
+    var objectUser = new User(
+        obj.name, 
+        obj.gender, 
+        obj.birth, 
+        obj.country, 
+        obj.email, 
+        obj.password, 
+        obj.photo, 
+        obj.admin
+    );
+
+    addLine(objectUser)
+    counterRows();    
 });
 
 
