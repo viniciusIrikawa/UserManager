e8e3d8ac1a768aba687eb7d867e5b9998b0415d2
Vinicius <viniciusiandrade@hotmail.com>
2022-01-02 00:07:35 -0300

    Refactoring

---
+++ controller/UserController.js
@@ -0,0 +1,78 @@
+class UserController{
+
+    constructor(formId, tableId){
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
+        
+    }   
+    counterRows(){
+        const counterUsers = document.querySelector('#counter-users')
+        const tbody = document.querySelector('#table-users').rows.length
+    
+        counterUsers.innerHTML = tbody;
+    
+    }
+    
+    onSubmit(){
+        this.formEl.addEventListener('submit', (event) => {
+            event.preventDefault();
+            this.addLine(this.getValues())
+            
+            this.counterRows();    
+        });
+        
+    }
+    addLine(dataUser){
+
+        this.tableEl.innerHTML= `
+        <tr>
+            <td>
+                <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`
+
+    }
+
+    getValues(){
+        let obj = {};
+
+        [...this.formEl.elements].forEach(function (field, index){
+            if(field.name == 'gender'){
+                if(field.checked){
+                    obj[field.name] = field.value;
+                }
+
+            }else{
+                obj[field.name] = field.value;
+            }
+        });
+            
+        return new User(
+            obj.name, 
+            obj.gender, 
+            obj.birth, 
+            obj.country, 
+            obj.email, 
+            obj.password, 
+            obj.photo, 
+            obj.admin
+        );
+    
+        // this.addLine(objectUser)
+
+    }
+
+
+
+
+}
