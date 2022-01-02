97ba1c0e255b1e07e81328f36f28afc990156e80
Vinicius <viniciusiandrade@hotmail.com>
2022-01-02 00:21:15 -0300

    Image update

--- controller/UserController.js
+++ controller/UserController.js
@@ -18,18 +18,48 @@ class UserController{
     onSubmit(){
         this.formEl.addEventListener('submit', (event) => {
             event.preventDefault();
-            this.addLine(this.getValues())
-            
             this.counterRows();    
+            
+            let values = this.getValues();
+
+            this.getPhoto((content) => {
+                values.photo = content
+
+                this.addLine(values)
+
+            });
+
+            
         });
         
     }
+    getPhoto(callback){
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+            if (item.name === 'photo'){
+                return item;
+
+            }
+
+        })
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
     addLine(dataUser){
 
         this.tableEl.innerHTML= `
         <tr>
             <td>
-                <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
             </td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
